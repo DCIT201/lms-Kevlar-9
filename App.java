3d80dfd9a1c647d438f7e559b7fff8ac6579d4ef
Kevlar-9 <fsiripi@st.ug.edu.gh>
2024-12-19 00:12:57 +0000

    Add files via upload

---
+++ App.java
@@ -0,0 +1,55 @@
+package org.example;
+
+public class App {
+    public static void main(String[] args) {
+        Library library = new Library();
+
+        // Adding books to the library
+        library.addBook(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 1997));
+        library.addBook(new Book("A Contemporary Confessions for Modern Gnostics", "Dylan Pahman", 2014));
+        library.addBook(new Book("To Kill a Mockingbird", "Harper Lee", 1960));
+        library.addBook(new Book("The Third Policeman", "Flann O'Brien", 1967));
+        library.addBook(new Book("Towards Colonial Freedom: Africa in the Struggle Against World Imperialism", "Kwame Nkrumah", 1969));
+        library.addBook(new Book("Contemporary Confessions ", "Dylan Pahman", 2014));
+        
+        String star = "******************************";
+
+        // Displaying the list of books in the library
+        System.out.println(star);
+        System.out.println("       Books in the Library");
+        System.out.println(star);
+        library.listBooks();
+        System.out.println();
+
+        // Creating a patron and borrowing a book
+        Patron patron = new Patron("Kwaku amane", 1001);
+
+        Book bookToBorrow = library.findBookByTitle("To Kill a Mockingbird");
+        patron.borrowBook(bookToBorrow, library);
+        System.out.println();
+
+        // Displaying the list of borrowed books
+        System.out.println(star);
+        System.out.println("   Borrowed Books");
+        System.out.println(star);
+        patron.listBorrowedBooks();
+        System.out.println();
+
+        // Returning the borrowed book
+        patron.returnBook(bookToBorrow, library);
+        System.out.println();
+
+        // Displaying the list of books in the library after operations
+        System.out.println(star);
+        System.out.println("Books in the Library After Operations");
+        System.out.println(star);
+        library.listBooks();
+        System.out.println();
+
+        // Displaying the detailed list of books with titles and years
+        System.out.println(star);
+        System.out.println("   Detailed List of Books");
+        System.out.println(star);
+        library.listBooksTitlesAndYears();
+    }
+}
