bcb796fa902f0ade0cd7edf102abb70e95f740d7
Kevlar-9 <fsiripi@st.ug.edu.gh>
2024-12-19 08:43:29 +0000

    Francis Siripi

--- dcit201-classroom-lms-LMS/app/src/main/java/org/example/Book.java
+++
@@ -1,9 +0,0 @@
-
-package org.example;
-
-/**
- * A record to represent a book.
- */
-public record Book(String title, String author, int yearPublished) {
-    // The constructor and field initialization are automatically handled by the record feature.
-}
